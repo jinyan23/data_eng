9ae12a9e755e595dd92d061ca29c1b6a8a14156c
jinyanzhang <zhangjinyan@outlook.sg>
2025-09-27 22:10:49 +0800

    create user-defined logger class in util.py

--- src/util.py
+++ src/util.py
@@ -3,6 +3,7 @@
 '''Utility Functions'''
 
 import zipfile
+import logging
 import os
 import yaml
 from datetime import datetime as dt
@@ -56,3 +57,59 @@ def time_now():
     time_now_str = time_now_dt + '\t'
 
     return time_now_str
+
+class UDLogger:
+    '''
+    User defined logger class.
+    Parameters
+    ----------
+        filename (str): .log file the log messages to be written to
+        name (str): name of the logger (typically the module's name)
+    '''
+    def __init__(self, filename: str, name: str):
+        self.filename = os.path.expanduser(f'~/logs/{filename}')
+        self.mode = 'a'
+        self.encoding = 'utf-8'
+        self.name = name
+
+    def create_logger(self):
+        '''
+        Create new logger. 
+
+        Parameters
+        ----------
+            name (str): logger name
+        '''
+        logger = logging.getLogger(self.name)
+        logger.setLevel(logging.INFO)
+
+        # create handlers
+        console_handler = logging.StreamHandler()
+        info_handler = logging.FileHandler(
+            filename=self.filename,
+            mode=self.mode,
+            encoding=self.encoding
+        )
+
+        # logging format
+        formatter = logging.Formatter(
+            fmt='%(asctime)s: %(levelname)s:%(name)s:%(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S'
+        ) 
+
+        # assignment of formatter to handler and handler to logger
+        console_handler.setFormatter(formatter)
+        info_handler.setFormatter(formatter)
+
+        logger.addHandler(console_handler)
+        logger.addHandler(info_handler)
+
+        return logger
+
+
+ud_logger = UDLogger(filename='import.log', name=__name__)
+
+logger = ud_logger.create_logger()
+
+logger.info('look at my message!')
+logger.error('look at my error!')
