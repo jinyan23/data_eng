799ce0263ca3d68753dede3f49d53c0d4178d2b8
jinyanzhang <zhangjinyan@outlook.sg>
2026-01-31 15:53:43 +0800

    fix test

--- pipeline/tests/integration/test_pv_train.py
+++ pipeline/tests/integration/test_pv_train.py
@@ -8,6 +8,14 @@ import zipfile
 
 from lta.pv_train import PVTrain
 
+@pytest.fixture(autouse=True)
+def mock_env(monkeypatch):
+    env = {
+        'LTA_KEY': 'api_token',
+    }
+    for k, v in env.items():
+        monkeypatch.setenv(k, v)
+    return env
 
 # api_call() test
 @responses.activate
