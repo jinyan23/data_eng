ac5c848157671959a08aae6f02c90032952e9283
jinyanzhang <zhangjinyan@outlook.sg>
2026-03-01 13:21:50 +0800

    #6: minimal example with airflow 3.1

--- airflow/dags/transport_dag.py
+++ airflow/dags/transport_dag.py
@@ -1,24 +1,22 @@
 # import the libraries
 
 from datetime import timedelta, datetime
-from airflow.models import DAG
+from airflow.sdk import DAG
 from airflow.providers.standard.operators.bash import BashOperator
 
 # defining DAG arguments
 default_args = {
     'owner': 'jyzhang',
     'start_date': datetime(2025, 11, 1),
-    'email': ['email@email.com'],
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
 
 # define the DAG
 dag = DAG(
-    'transport_dag',
+    dag_id='transport_dag',
     default_args=default_args,
-    description='Transport DAG',
-    schedule=timedelta(days=1),
+    description='Transport DAG'
 )
 
 # define the first task
@@ -27,3 +25,5 @@ extract = BashOperator(
     bash_command='echo "hello world"',
     dag=dag,
 )
+
+extract
